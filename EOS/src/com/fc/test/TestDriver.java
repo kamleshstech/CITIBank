3e4fb8dfe73ac3764683868c0332837ac687fc7d
Dev1 <Dev1@gmail.com>
2020-04-11 19:44:10 +0530

    modified TestDriver class and performing testing on it

--- EOS/src/com/fc/test/TestDriver.java
+++ EOS/src/com/fc/test/TestDriver.java
@@ -1,10 +1,13 @@
 package com.fc.test;
 
+import com.fc.util.DateUtil;
+
 public class TestDriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		DateUtil dateUtil = new DateUtil();
+		System.out.println(dateUtil.getDate());
 	}
 
 }
