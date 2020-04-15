ee45b41ae101e9425c44f1814bed60b3cac34eb2
Dev1 <Dev1@gmail.com>
2020-04-15 16:36:08 +0530

    Added Connection class and also made the changes in the ConnectionProvider class as per the req of the new Connection class

---
+++ EOS/src/com/fc/connection/provider/Connection.java
@@ -0,0 +1,11 @@
+package com.fc.connection.provider;
+
+public class Connection {
+	
+	public Connection(String a, String b, String c, String d) {}
+		
+	public Statement createStatement(Connection con) {
+		
+		return new Statement();
+	} 
+}
--- EOS/src/com/fc/connection/provider/ConnectionProvider.java
+++ EOS/src/com/fc/connection/provider/ConnectionProvider.java
@@ -1,31 +1,28 @@
 package com.fc.connection.provider;
 
-import java.sql.Connection;
 
 public class ConnectionProvider {
+	
+	private Connection con;
+	
 	private String url;
 	private String userName;
 	private String pwd;
 	private String driverClass;
 	
-	
-	public ConnectionProvider(String url, String userName, String pwd, String driverClass) {
-		this.url = url;
-		this.userName = userName;
-		this.pwd = pwd;
-		this.driverClass = driverClass;
-	}
 
 
 	public Connection getConnection() throws Exception { 
 		
-		ConnectionProvider connectionProvider = new ConnectionProvider("url", "userName", "pwd", "driverClass");
+		Connection con = new Connection("url", "userName", "pwd", "driverClass");
 		
-		return (Connection) connectionProvider;
+		return con;
 	}
 	
 	public Statement createStatement(Connection con) {
 		
-		return new Statement();
+		Statement stmt = con.createStatement(con);
+		
+		return stmt;
 	}
 }
