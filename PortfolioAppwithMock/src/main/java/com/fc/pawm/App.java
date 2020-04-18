ca1e219a2375390ca138baf907dbe74e863ac585
TL <TL@gmail.com>
2020-04-18 18:24:29 +0530

    PortfolioAppWithMock

---
+++ PortfolioAppwithMock/src/main/java/com/fc/pawm/App.java
@@ -0,0 +1,13 @@
+package com.fc.pawm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
